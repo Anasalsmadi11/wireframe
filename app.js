9b625097cef867d00d376095833144a7be727c19
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2021-04-01 17:51:53 +0300

    adding app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+/* alert("hello")
+alert(4**5) */
+
+var p = "Welcomr to my first page"
+alert (p)
+
+var x = prompt("please enter a number","put a number igher or smaller than 7");
+//alert(x + 4) //just in sum sign the number 7 above is treared like a string
+
+//other operations the number 7 is treated like a number
+
+//alert(x * 4)
+//alert(x % 4)
+//alert(x / 4)
+
+if (x >= 7) {
+    document.write("have a nice weekend")
+}
+  else{
+    document.write("good luck with study")
+}
+
+
+
